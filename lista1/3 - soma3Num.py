7908e7747e19f36429e8c9eefe0c2e822bd5718e
hilgabe <hilsongabrielcarvalho@gmail.com>
2025-10-24 20:53:49 -0300

    Adicionando exercicio 3

---
+++ lista1/3 - soma3Num.py
@@ -0,0 +1,10 @@
+#3. Peca ao usuario para digitar tr ´ es valores inteiros e imprima a soma deles.
+
+print("Digite três números: ")
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+n3 = int(input("Terceiro número: "))
+
+total = (n1+n2+n3)
+
+print('A soma de {}, {} e {} é: {}'.format(n1, n2, n3, total))
