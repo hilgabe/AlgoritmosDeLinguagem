e9f518847302af081db2948b0686d775327089b1
hilgabe <hilsongabrielcarvalho@gmail.com>
2025-10-24 21:03:09 -0300

    Adicionando exercicio 4

---
+++ lista1/4 - quadradoNumReal.py
@@ -0,0 +1,5 @@
+#4. Leia um numero real e imprima o resultado do quadrado desse numero.
+
+num = float(input("Digite um número real para descobrir o seu quadrado: "))
+quadrado = num ** 2 
+print("O quadrado do número informado é:", quadrado)
