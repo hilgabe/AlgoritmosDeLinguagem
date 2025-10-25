d1e7d6ec636af7ab1f4321bc5e7a652418456aba
hilgabe <hilsongabrielcarvalho@gmail.com>
2025-10-24 21:10:50 -0300

    Adicionando exercicio 5

---
+++ lista1/5 - quintaParteNumReal.py
@@ -0,0 +1,6 @@
+#5. Leia um numero real e imprima a quinta parte deste numero.
+
+num = float(input("Digite um número real para descobrir a sua quinta parte: ")) 
+quinta_parte = num / 5
+
+print("A quinta parte do número informado é:", quinta_parte)
